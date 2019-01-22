bb9ca7a091242519f36dba6ec877a0b40ef9cd12
Davis Bray - xena16 <pbray@trinity.edu>
2019-01-22 10:38:36 -0600

    I created an Entity class that consists of a location and size that includes a intersection method and a companion object with tests

---
+++ src/main/scala/basics/Entity.scala
@@ -0,0 +1,12 @@
+package basics
+
+class Entity {
+  private var x = 0d
+  private var y = 0d
+  private var width = 1d
+  private var height = 1d
+  
+  def intersects(e: Entity): Boolean = {
+    ???
+  }
+}
