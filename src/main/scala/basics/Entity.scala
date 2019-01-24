cd0939ca5cfebf61f6c959386323d4b7f96f6e98
Davis Bray - atlas09 <pbray@trinity.edu>
2019-01-24 12:45:31 -0600

    Added code from Jan 22nd and 24th class adding an entity class with and intersection method, aswell as a basic Room, with outlined methods, and Item class for the MUD.

--- src/main/scala/basics/Entity.scala
+++ src/main/scala/basics/Entity.scala
@@ -1,12 +1,24 @@
 package basics
 
-class Entity {
-  private var x = 0d
-  private var y = 0d
-  private var width = 1d
-  private var height = 1d
+class Entity(private var x = 0d
+  private var y = 0d) {
+  
+  private var width = 1.0
+  private var height = 1.0
   
   def intersects(e: Entity): Boolean = {
-    ???
+    val overlapX = (x - e.x).abs < (width + e.width)/2
+    val overlapY = (y - e.y).abs < (height + e.height)/2
+    overlapX && overlapY
+  }
+}
+
+object Entity {
+  def main(args: Array[String]): Unit = {
+    val e1 = new Entity(0,0)
+    val e2 = new Entity(0,0)
+    println(e1.intersects(e2))
+    val e3 = new Entity(10,0)
+    println(e1.intersects(e3))
   }
 }
---
+++ src/main/scala/mud/Room.scala
@@ -0,0 +1,22 @@
+import scala.xml.XML
+
+package mud
+
+class Room(val name: String, 
+    private val desc: String,
+    private var items: List[Item],
+    private val exits: Array[Int]){ //Private description cause description needs build
+  def description(): String = ???
+  def getExit(dir: Int): Option[Room] = ???
+  def getItem(itemName: String): Option[Item] = ???
+  def dropItem(item: Item): Unit = ???
+}
+
+object Room {
+  val room = readRooms()
+  
+  def readRooms(): Array[Room] = {
+    val xml = XML.loadFile("map.xml")
+    ???
+  }
+}
