20be67abfe8721c86d5364dbba8e64557512f51e
Kaltag <pdavisbray@gmail.com>
2019-01-31 14:22:29 -0600

    Fixed MutableRectangle

--- src/main/scala/polymorphism/MutableRectangle.scala
+++ src/main/scala/polymorphism/MutableRectangle.scala
@@ -2,7 +2,7 @@ package polymorphism
 
 import scalafx.scene.paint.Color
 
-class MutableRectangle(protected var _width: Double, protected var _height: Double, private var _color: Color) extends Shape with HasColor {
+class MutableRectangle(protected var _width: Double, protected var _height: Double, private var _color: Color) extends Shape {
                       // Protected so that subtypes can see it
   def width = _width
   def width_=(w: Double) = _width = w
@@ -10,7 +10,7 @@ class MutableRectangle(protected var _width: Double, protected var _height: Doub
   def height = _height
   def height_=(h: Double) = _height = h
   
-  def color = _color
+  override def color = _color
   def color_=(c: Color) = _color = c
   
   override def area: Double = width * height
